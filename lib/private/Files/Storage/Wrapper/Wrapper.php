038dbb6d915e642bdec00a736754b28cbf203865
Thomas Müller <thomas.mueller@tmit.eu>
2018-07-13 16:53:35 +0200

    fopen on IStorage, View and Node API is deprecated - these operations will throw an exception as replacement readFile and writeFile are introduced
    This changes the API semantics from a POSIX file handle schema to a GET/PUT schema which is more suitable for modern storages like S3

--- lib/private/Files/Storage/Wrapper/Wrapper.php
+++ lib/private/Files/Storage/Wrapper/Wrapper.php
@@ -29,6 +29,7 @@ namespace OC\Files\Storage\Wrapper;
 use OCP\Files\InvalidPathException;
 use OCP\Files\Storage\ILockingStorage;
 use OCP\Lock\ILockingProvider;
+use Psr\Http\Message\StreamInterface;
 
 class Wrapper implements \OC\Files\Storage\Storage, ILockingStorage {
 	/**
@@ -291,7 +292,7 @@ class Wrapper implements \OC\Files\Storage\Storage, ILockingStorage {
 	 * @return resource
 	 */
 	public function fopen($path, $mode) {
-		return $this->getWrapperStorage()->fopen($path, $mode);
+		throw new \BadMethodCallException('fopen is no longer allowed to be called');
 	}
 
 	/**
@@ -612,5 +613,25 @@ class Wrapper implements \OC\Files\Storage\Storage, ILockingStorage {
 		if ($storage->instanceOfStorage('\OCP\Files\Storage\ILockingStorage')) {
 			$storage->changeLock($path, $type, $provider);
 		}
+	}
+
+	/**
+	 * @param string $path
+	 * @param array $options
+	 * @return StreamInterface
+	 * @since 11.0.0
+	 */
+	public function readFile(string $path, array $options = []): StreamInterface {
+		return $this->getWrapperStorage()->readFile($path, $options);
+	}
+
+	/**
+	 * @param string $path
+	 * @param StreamInterface $stream
+	 * @return int
+	 * @since 11.0.0
+	 */
+	public function writeFile(string $path, StreamInterface $stream): int {
+		return $this->getWrapperStorage()->writeFile($path, $stream);
 	}
 }
